ef4e11440581b96b35793b441823c749a5ac06d3
Pedro Henrique <phznff13@gmail.com>
2025-08-11 16:55:42 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LoginRoute from './routes/LoginRoute';
+
+const App: React.FC = () => {
+  return(
+      <div>
+        <LoginRoute />
+      </div>
+  )
+}
+
+export default App;
