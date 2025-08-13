8972fe604eac0d933b6a66eeb4ac0d2b1f32ccd5
PedroHenriquee777 <phznff13@gmail.com>
2025-08-13 14:41:56 -0300

    feat: responsividade e melhorias de codigo

--- src/view/pages/login/components/LoginForm.tsx
+++ src/view/pages/login/components/LoginForm.tsx
@@ -1,62 +1,51 @@
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { toast } from "sonner";
 
 export function LoginForm() {
-	function handleSubmit(e) {
-		e.preventDefault();
-	}
-	return (
-		<section className="w-full">
-			<h1 className="text-4xl break-words w-120 md:text-7xl font-extrabold text-gray-800 leading-tight mb-3">
-				Sign in with <br /> password
-			</h1>
-			<p className="text-1xl text-gray-500 mb-6">
-				Don't have an account?{" "}
-				<a className="text-blue-400 hover:underline" href="#">
-					Sign up
-				</a>
-			</p>
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <section className="w-full">
+      <h1 className="text-5xl md:text-6xl break-words font-extrabold text-gray-800 leading-tight mb-3">
+        Sign in with password
+      </h1>
+      <p className="text-lg text-gray-500 mb-6">
+        Don't have an account?{" "}
+        <a className="text-blue-500 hover:text-blue-600 hover:underline transition-colors" href="#">
+          Sign up
+        </a>
+      </p>
 
-			<form className="space-y-2 flex flex-col" onSubmit={handleSubmit}>
-				<Input
-					type="email"
-					placeholder="Email"
-					className="w-150 h-15 rounded-xl border border-gray-300 px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-200"
-				/>
-				<Input
-					type="password"
-					placeholder="Password"
-					className="w-150 h-15 mt-3 rounded-xl border border-gray-300 px-3 py-2 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-200"
-				/>
+      <form className="space-y-3 flex flex-col md:ml-0" onSubmit={handleSubmit}>
+        <Input type="email" placeholder="Email" className="h-12" />
+        <Input type="password" placeholder="Password" className="h-12" />
 
-				<div className="flex flex-col ml-120 mt-2 mb-2">
-					<a
-						href="#"
-						className="text-sm text-center border-none rounded border text-[#001838] bg-gray-200 px-2 py-2 w-30"
-					>
-						Reset password
-					</a>
-				</div>
+        <div className="flex justify-end mt-2 mb-2">
+          <a href="#">
+            <Button variant="ghost"> Reset password </Button>
+          </a>
+        </div>
 
-				<Button
-					variant="outline"
-					onClick={() =>
-						toast.success("Login successfully!", {
-							description: "We're carrying your credentials...",
-							action: {
-								label: "X",
-								onClick: () => console.log("X"),
-							},
-						})
-					}
-					type="submit"
-					className="w-150 h-15 gap-10 mt-2 bg-[#001838] md:text-xl cursor-pointer text-white py-3 rounded-xl text-sm font-semibold hover:bg-[#000e21] hover:text-white transition"
-				>
-					SIGN IN
-				</Button>
-			</form>
-			<div className="mt-6"></div>
-		</section>
-	);
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast.success("Login successfully!", {
+              description: "We're carrying your credentials...",
+              action: {
+                label: "X",
+                onClick: () => console.log("X"),
+              },
+            })
+          }
+          type="submit"
+          className=" w-full max-w-4xl h-12 mt-2 bg-[#001838] md:text-xl cursor-pointer text-white py-3 rounded-xl text-sm font-semibold hover:bg-[#000e21] hover:text-white transition "
+        >
+          SIGN IN
+        </Button>
+      </form>
+      <div className="mt-6"></div>
+    </section>
+  );
 }
--- src/view/pages/login/index.tsx
+++ src/view/pages/login/index.tsx
@@ -4,18 +4,18 @@ import TradeWolfLogo from "../../../assets/img/LogoBG.png";
 
 export function LoginPage() {
 	return (
-		<div className="flex h-screen overflow-hidden">
+		<div className="flex h-screen overflow-hidden ">
 			<div className="hidden md:flex bg-[#001838] text-white flex-col items-center justify-center w-160">
 				<img
 					src={TradeWolfLogo}
 					alt="TradeWolfLogo"
-					className="w-97.5 h-97.5 mb-3"
+					className="w-96 h-96 mb-3"
 				/>
 				<h1 className="text-4xl font-bold">TradeWolf</h1>
 			</div>
 
-			<div className="flex flex-col justify-start items-start flex-1 -mr-20 mt-7 px-6 md:px-16">
-				<div className="w-full flex flex-col mt-20">
+			<div className="flex flex-col justify-start items-start flex-1 -mr-5 mt-7 px-6 pr-13 md:px-16 md:ml-0">
+				<div className="w-full max-w-xl flex flex-col mt-20">
 					<LoginForm />
 					<SocialLoginIcons />
 				</div>
