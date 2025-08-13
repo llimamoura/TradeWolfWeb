d49557d8e9e7bd2d3e968a1eef37f8176eb94e42
PedroHenriquee777 <phznff13@gmail.com>
2025-08-13 14:41:56 -0300

    feat: responsividade e melhorias de codigo

--- src/view/pages/login/components/SocialLoginIcons.tsx
+++ src/view/pages/login/components/SocialLoginIcons.tsx
@@ -1,27 +1,37 @@
+import { Button } from "@/components/ui/button";
+import apple from "../../../../assets/icons/apple.png";
 import facebook from "../../../../assets/icons/facebook.png";
 import google from "../../../../assets/icons/google.png";
-import apple from "../../../../assets/icons/apple.png";
 
 export function SocialLoginIcons() {
-	return (
-		<div className="flex flex-col">
-			<div className="flex items-center w-150 my-6">
-				<hr className="flex-grow border-gray-300" />
-				<span className="mx-3 text-gray-500 text-sm">or continue with</span>
-				<hr className="flex-grow border-gray-300" />
-			</div>
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex items-center my-6 mb-8">
+        <hr className="flex-grow border-gray-300" />
+        <span className="mx-3 text-gray-500 text-sm">or continue with</span>
+        <hr className="flex-grow border-gray-300" />
+      </div>
 
-			<div className="flex gap-8 justify-center mr-78">
-				<button className="cursor-pointer pl-12 pr-12 pt-2 pb-2 border rounded bg-gray-100 hover:bg-gray-300 border-none transition">
-					<img src={facebook} alt="Facebook" className="w-10 h-10" />
-				</button>
-				<button className="cursor-pointer pl-12 pr-12 pt-1 pb-1 border rounded bg-gray-100 hover:bg-gray-300 border-none transition">
-					<img src={google} alt="Google" className="w-10 h-10" />
-				</button>
-				<button className="cursor-pointer pl-12 pr-12 pt-1 pb-1 border rounded bg-gray-100 hover:bg-gray-300 border-none transition">
-					<img src={apple} alt="Apple" className="w-10 h-10" />
-				</button>
-			</div>
-		</div>
-	);
+      <div className="flex gap-4 w-full justify-center">
+        <Button
+          type="button"
+          className="bg-gray-100 hover:bg-gray-300 flex-1 h-fit"
+        >
+          <img src={facebook} alt="Facebook" className="size-8" />
+        </Button>
+        <Button
+          type="button"
+          className="bg-gray-100 hover:bg-gray-300 flex-1 h-fit"
+        >
+          <img src={google} alt="Google" className="size-8" />
+        </Button>
+        <Button
+          type="button"
+          className="bg-gray-100 hover:bg-gray-300 flex-1 h-fit"
+        >
+          <img src={apple} alt="Apple" className="size-8"/>
+        </Button>
+      </div>
+    </div>
+  );
 }
