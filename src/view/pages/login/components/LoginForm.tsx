05cb048fec6baf753e2ced2ca6a3fcc2623ab736
PedroHenriquee777 <phznff13@gmail.com>
2025-08-18 19:43:47 -0300

    mais testes de responsividade e layout

--- src/view/pages/login/components/LoginForm.tsx
+++ src/view/pages/login/components/LoginForm.tsx
@@ -9,13 +9,13 @@ export function LoginForm() {
 
   return (
     <section className="w-full mt-20 ">
-      <h1 className="text-5xl hidden md:block md:text-4xl font-extrabold text-gray-800 leading-tight mb-3">
+      <h1 className="text-5xl hidden md:block md:text-3xl font-extrabold text-gray-900 leading-tight mb-3">
         Sign in with password
       </h1>
-      <h1 className="text-4xl block  md:hidden font-extrabold text-black leading-tight mb-14">
+      <h1 className="text-4xl block  md:hidden font-extrabold text-gray-900 leading-tight mb-14">
         Let's log you in
       </h1>
-      <p className="hidden md:block text-lg text-gray-500 mb-6">
+      <p className="hidden md:block text-sm text-gray-500 mb-6">
         Don't have an account?{" "}
         <a
           className="text-blue-500 hover:text-blue-600 hover:underline transition-colors"
@@ -29,9 +29,9 @@ export function LoginForm() {
         <Input type="email" placeholder="Email" className="h-12" />
         <Input type="password" placeholder="Password" className="h-12" />
 
-        <div className="justify-end mt-2 mb-2 hidden md:flex">
+        <div className="justify-end mt-2 mb-2 hidden md:flex text-blue-950">
           <a href="#">
-            <Button variant="ghost">Reset password</Button>
+            <Button variant="ghost" className="bg-gray-200 hover:bg-gray-300 hover:text-blue-950">Reset password</Button>
           </a>
         </div>
 
