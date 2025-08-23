bda6c143e4043c3fa0a5bdd20fcf9d100ab5fe89
PedroHenriquee777 <phznff13@gmail.com>
2025-08-23 16:30:17 -0300

    refatoracao dos estilos

--- src/view/pages/forgotPassword/ResetPassword.tsx
+++ src/view/pages/forgotPassword/ResetPassword.tsx
@@ -1,23 +1,23 @@
+import { Toaster } from "@/components/ui/sonner";
 import TradeWolfLogo from "../../../assets/img/LogoBG.png";
 import { ResetPasswordComponent } from "./components/ResetPasswordComponent";
-import { Toaster } from "@/components/ui/sonner";
 
 export function ResetPasswordPage() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Toaster richColors position="top-center" />
 
-      <div className="hidden lg:flex bg-primary bg-gradient-to-b from-primary to-[#004EB5] text-primary-foreground flex-col items-center justify-center w-140">
+      <div className="hidden lg:flex bg-primary bg-gradient-to-b from-primary to-tertiary text-primary-foreground flex-col items-center justify-center w-140">
         <img
           src={TradeWolfLogo}
           alt="TradeWolf logo"
           aria-label="TradeWolf logo"
-          className="w-96 h-96 mb-3"
+          className="w-110 h-110 mb-3"
         />
-        <h2 className="text-4xl font-bold">TradeWolf</h2>
+        <h2 className="text-5xl font-bold">TradeWolf</h2>
       </div>
 
-      <div className="flex flex-col justify-center items-center flex-1 px-6 md:px-16">
+      <div className="h-screen flex flex-col justify-center items-center flex-1 px-6 md:px-16">
         <div className="w-full max-w-xl flex flex-col">
           <ResetPasswordComponent />
         </div>
--- src/view/pages/forgotPassword/VerifyCode.tsx
+++ src/view/pages/forgotPassword/VerifyCode.tsx
@@ -4,17 +4,17 @@ import { VerifyCodeComponent } from "./components/VerifyCodeComponent";
 export function VerifyCodePage() {
   return (
     <div className="flex h-screen overflow-hidden">
-      <div className="hidden lg:flex bg-primary bg-gradient-to-b from-primary to-[#004EB5] text-primary-foreground flex-col items-center justify-center w-140">
+      <div className="hidden lg:flex bg-primary bg-gradient-to-b from-primary to-tertiary text-primary-foreground flex-col items-center justify-center w-140">
         <img
           src={TradeWolfLogo}
           alt="TradeWolf logo"
           aria-label="TradeWolf logo"
-          className="w-96 h-96 mb-3"
+          className="w-110 h-110 mb-3"
         />
-        <h2 className="text-4xl font-bold">TradeWolf</h2>
+        <h2 className="text-5xl font-bold">TradeWolf</h2>
       </div>
 
-      <div className="flex flex-col justify-center items-center flex-1 px-6 md:px-16">
+      <div className="h-screen flex flex-col justify-center items-center flex-1 px-6 md:px-16">
         <div className="w-full max-w-xl flex flex-col">
           <VerifyCodeComponent />
         </div>
