38eec132f57a9b3e2df94719cf863e246909c111
PedroHenriquee777 <phznff13@gmail.com>
2025-08-24 23:34:29 -0300

    criacao das páginas de comprovante de residencia e testando combobox do shadcn

--- src/view/pages/login/index.tsx
+++ src/view/pages/login/index.tsx
@@ -5,18 +5,17 @@ import { SocialLoginIcons } from "./components/SocialLoginIcons";
 export function LoginPage() {
   return (
     <div className="flex h-screen overflow-hidden">
-      <div className="hidden lg:flex bg-primary text-primary-foreground flex-col items-center justify-center w-140">
+      <div className="hidden lg:flex bg-primary bg-gradient-to-b from-primary to-tertiary text-primary-foreground flex-col items-center justify-center w-140">
         <img
           src={TradeWolfLogo}
           alt="TradeWolf logo"
           aria-label="TradeWolf logo"
-          className="w-96 h-96 mb-3"
+          className="w-110 h-110 mb-3"
         />
-        <h2 className="text-4xl font-bold">TradeWolf</h2>
+        <h2 className="text-5xl font-bold">TradeWolf</h2>
       </div>
-
-      <div className="flex flex-col justify-start items-center lg:items-start flex-1 -mr-5 mt-7 px-6 pr-13 md:px-16 lg:ml-0">
-        <div className="w-full max-w-xl flex flex-col mt-20">
+      <div className="h-screen flex flex-col justify-center items-center flex-1 px-6 md:px-16 bg-white rounded-l-4xl -ml-0 lg:-ml-7">
+        <div className="w-full max-w-xl flex flex-col">
           <LoginForm />
           <SocialLoginIcons />
         </div>
