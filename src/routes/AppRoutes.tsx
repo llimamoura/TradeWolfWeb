4fc1c9c4de952efda7cd80ef2a5af22a888fec6e
PedroHenriquee777 <phznff13@gmail.com>
2025-09-08 16:51:32 -0300

    feat: nested routes em AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,32 +8,24 @@ import { ProfilePage } from "@/view/pages/signup/Profile";
 import { ConfirmResidencyPage } from "@/view/pages/signup/Residency";
 import { ProofResidencyPage } from "@/view/pages/signup/Residency/ProofResidency";
 import { LoginPage } from "../view/pages/login";
-import { ProfileLayout } from "@/view/layouts/ProfileLayout";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
         <Route index element={<LoginPage />} />
-        <Route path="forgot-password" element={<ForgotPasswordPage />} />
-        <Route
-          path="forgot-password/verification"
-          element={<VerifyCodePage />}
-        />
-        <Route
-          path="forgot-password/reset-password"
-          element={<ResetPasswordPage />}
-        />
+        <Route path="forgot-password">
+          <Route index element={<ForgotPasswordPage />} />
+          <Route path="verification" element={<VerifyCodePage />} />
+          <Route path="reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
-        <Route path="/create-user" element={<ConfirmResidencyPage />} />
-        <Route
-          path="/create-user/proof-residency"
-          element={<ProofResidencyPage />}
-        />
-        <Route path="/create-user/identify" element={<IdentifyPage />} />
-      </Route>
-      <Route path="/" element={<ProfileLayout />}>
-        <Route path="/create-user/profile" element={<ProfilePage />} />
+        <Route path="sign-up">
+          <Route index element={<ConfirmResidencyPage />} />
+          <Route path="proof-residency" element={<ProofResidencyPage />} />
+          <Route path="identify" element={<IdentifyPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
       </Route>
     </Routes>
   );
