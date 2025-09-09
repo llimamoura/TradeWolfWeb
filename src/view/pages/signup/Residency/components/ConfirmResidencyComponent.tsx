a9cde490adf51186e433e3e109e00f1c70c99aa3
PedroHenriquee777 <phznff13@gmail.com>
2025-09-09 15:42:02 -0300

    fix: diminuição de espacamento de texto

--- src/view/pages/signup/Residency/components/ConfirmResidencyComponent.tsx
+++ src/view/pages/signup/Residency/components/ConfirmResidencyComponent.tsx
@@ -9,7 +9,7 @@ export function ConfirmResidencyComponent() {
       <h1 className="flex justify-center lg:mb-6 text-3xl lg:text-4xl lg:text-center text-start font-extrabold text-foreground leading-tight mb-5">
         Let's Verify Your Identity
       </h1>
-      <p className="font-medium lg:text-center text-start text-sm text-muted-foreground mb-16 lg:mb-25">
+      <p className="font-medium lg:text-center text-start text-sm text-muted-foreground mb-16 lg:mb-20">
         We are required to verify your identity before you can use the
         application. Your information will be encrypted and stored securely.
       </p>
