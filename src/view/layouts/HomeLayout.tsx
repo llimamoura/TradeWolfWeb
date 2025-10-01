6caa812ffddc33aa4633706c240f7c6483524c5c
PedroHenriquee777 <phznff13@gmail.com>
2025-10-01 16:34:12 -0300

    fix: estilos da Home SideBar alterados

--- src/view/layouts/HomeLayout.tsx
+++ src/view/layouts/HomeLayout.tsx
@@ -2,7 +2,17 @@ import { Outlet } from "react-router-dom";
 import TradeWolfLogo from "../../assets/img/LogoBG.png";
 import TradeWolfBlueLogo from "../../assets/img/TradeWolfBlueLogo.png";
 import { Separator } from "@/components/ui/separator";
-import { House, ChartLine, CirclePlus, LogOut, Search, Bell, CircleUser, Settings, Hourglass } from "lucide-react";
+import {
+  House,
+  ChartLine,
+  CirclePlus,
+  LogOut,
+  Search,
+  Bell,
+  CircleUser,
+  Settings,
+  Hourglass,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export function HomeLayout() {
@@ -27,7 +37,7 @@ export function HomeLayout() {
         </div>
       </div>
 
-      <div className="hidden lg:flex bg-gradient-to-b from-primary to-tertiary text-primary-foreground rounded-2xl items-center flex-col m-3 xl:m-5 ml-6 xl:ml-15 w-20 xl:max-w-xl lg:w-28 min-h-6 py-6">
+      <div className="hidden lg:flex bg-gradient-to-r from-primary to-tertiary text-primary-foreground rounded-2xl items-center flex-col m-3 xl:m-5 ml-6 xl:ml-15 w-20 xl:max-w-xl lg:w-28 min-h-6 py-6 space-y-5">
         <img
           src={TradeWolfLogo}
           alt="TradeWolf logo"
@@ -35,35 +45,46 @@ export function HomeLayout() {
           className="size-14 lg:size-16 xl:size-25 mb-6"
         />
 
-        <Separator className="border mb-10" />
+        <Separator className="border mb-13" />
 
-        <Button variant="ghost" className="rounded-full mb-6 xl:mb-15">
+        <Button
+          variant="link"
+          className="rounded-full mb-6 xl:mb-16 text-background"
+        >
           <House className="size-6 lg:size-8 xl:size-10" />
         </Button>
 
-        <Button variant="ghost" className="rounded-full mb-6 xl:mb-15">
+        <Button
+          variant="link"
+          className="rounded-full mb-6 xl:mb-16 text-background"
+        >
           <ChartLine className="size-6 lg:size-8 xl:size-10" />
         </Button>
 
-        <Button variant="ghost" className="rounded-full mb-6 xl:mb-15">
+        <Button
+          variant="link"
+          className="rounded-full mb-6 xl:mb-16 text-background"
+        >
           <Hourglass className="size-6 lg:size-8 xl:size-10" />
         </Button>
 
-        <Button variant="ghost" className="rounded-full mb-6 xl:mb-15">
+        <Button
+          variant="link"
+          className="rounded-full mb-6 xl:mb-16 text-background"
+        >
           <Settings className="size-6 lg:size-8 xl:size-10" />
         </Button>
 
-        <Button variant="ghost" className="rounded-full mb-20 xl:mb-15">
-          <CirclePlus className="size-6 lg:size-8 xl:size-10 rounded-xl" />
+        <Button
+          variant="link"
+          className="rounded-full mb-20 xl:mb-10 text-background"
+        >
+          <CirclePlus className="size-6 lg:size-8 xl:size-13 rounded-xl" />
         </Button>
 
-        <Button variant="ghost" className="mt-auto rounded-full">
+        <Button variant="link" className="mt-auto rounded-full text-background">
           <LogOut className="size-6 lg:size-8 xl:size-10" />
         </Button>
-      </div>
-
-      <div className="flex-1 overflow-auto">
-        <Outlet />
       </div>
 
       <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-background text-center justify-center rounded-t-3xl text-primary-foreground border-t">
@@ -84,6 +105,10 @@ export function HomeLayout() {
             <Settings className="size-7 sm:size-10 text-primary" />
           </Button>
         </div>
+      </div>
+
+      <div className="flex-1 overflow-auto">
+        <Outlet />
       </div>
     </div>
   );
