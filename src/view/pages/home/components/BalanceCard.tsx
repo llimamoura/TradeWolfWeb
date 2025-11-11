644e07a27fb30fc899bbf54eb9f96e1d3c4c99b8
PedroHenriquee777 <phznff13@gmail.com>
2025-11-11 08:51:24 -0300

    fix: padding lateral em BalanceCard

--- src/view/pages/home/components/BalanceCard.tsx
+++ src/view/pages/home/components/BalanceCard.tsx
@@ -10,16 +10,18 @@ export function BalanceCard() {
   return (
     <Card className="flex justify-center w-full min-h-50 lg:min-h-60 xl:min-h-30 bg-card shadow-lg">
       <CardHeader>
-        <CardTitle className="font-extrabold text-search-dropdown text-xl md:text-3xl mb-13">
-          My balance
-        </CardTitle>
-        <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-2 sm:space-y-0 gap-5">
-          <CardDescription className="font-extrabold text-3xl md:text-4xl text-blue-muted">
-            $25,901.41
-          </CardDescription>
-          <CardContent className="p-2 text-sm text-background font-semibold bg-success rounded-lg">
-            810%
-          </CardContent>
+        <div className="xl:px-2 px-0">
+          <CardTitle className="font-extrabold text-search-dropdown text-xl md:text-3xl mb-10">
+            My balance
+          </CardTitle>
+          <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-2 sm:space-y-0 gap-5">
+            <CardDescription className="font-extrabold text-3xl md:text-4xl text-blue-muted">
+              $25,901.41
+            </CardDescription>
+            <CardContent className="p-2 text-sm text-background font-semibold bg-success rounded-lg">
+              810%
+            </CardContent>
+          </div>
         </div>
       </CardHeader>
     </Card>
