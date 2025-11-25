5614eeccaa1174adb9795933cf3d9955d226bf6f
PedroHenriquee777 <phznff13@gmail.com>
2025-11-25 16:40:47 -0300

    feat: adicionado formatação de moedas para USD

---
+++ src/utils/format-currency.ts
@@ -0,0 +1,6 @@
+export const formatCurrencyUSD = (value: number): string => {
+  return new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+};
